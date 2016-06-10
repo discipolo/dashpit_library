87b57680d80d815a32823cc48ad3cd71c84f43bd
Discipolo <discipolo@105174.no-reply.drupal.org>
2016-06-11 00:06:50 +0200

    add config class

--- src/config.php
+++ src/config.php
@@ -2,27 +2,24 @@
 /**
  * Created by PhpStorm.
  * User: buddy
- * Date: 10/06/16
- * Time: 19:18
+ * Date: 11/06/16
+ * Time: 00:01
  */
-namespace dashpit;
-use dashpit\Link;
 
+namespace dashpit;
 
 // http://symfony.com/doc/current/components/yaml/introduction.html#reading-yaml-files
 use Symfony\Component\Yaml\Yaml;
 use Symfony\Component\Yaml\Exception\ParseException;
 
-class LinkList
+class config
 {
     var $source;
-    function __construct($source) {
-        $this->source = $source;
-    }
-    public function getLinkList() {
+
+    public function getConfig($source) {
         $values = '';
         try {
-            $values = Yaml::parse(file_get_contents($this->source));
+            $values = Yaml::parse(file_get_contents($source));
         } catch (ParseException $e) {
             printf("Unable to parse the YAML string: %s", $e->getMessage());
         }
